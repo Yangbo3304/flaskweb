3c3b746f350d4b05a8e64a9afa6e139567ab5bcd
Bob Yang <2739296373@qq.com>
2017-11-22 14:12:42 +0800

    add templates

--- hello.py
+++ hello.py
@@ -1,17 +1,34 @@
 #!/usr/bin/evn python
 # -*- coding: UTF-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for, redirect
+from flask_moment import Moment
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_wtf import FlaskForm as Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+app.config['SECRET_KEY'] = "Hard to guess string"
 
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField("what is your name?", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
