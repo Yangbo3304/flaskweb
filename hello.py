e102f495b490740e2955b05edf4cdbade6d28052
Bob Yang <2739296373@qq.com>
2017-11-27 08:01:53 +0800

    update flash

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 #!/usr/bin/evn python
 # -*- coding: UTF-8 -*-
+import os
 from flask import Flask, flash
 from flask import render_template, session, url_for, redirect
 from flask_moment import Moment
@@ -8,14 +9,20 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm as Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "Hard to guess string"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
-app.config['SECRET_KEY'] = "Hard to guess string"
-
+db =SQLAlchemy(app)
 
 class NameForm(Form):
     name = StringField("what is your name?", validators=[DataRequired()])
